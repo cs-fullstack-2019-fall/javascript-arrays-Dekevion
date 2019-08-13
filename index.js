33b5286c8bf1870d1ccfffebc7caeab5707cee6b
Dekevion Wade <Dekevionwade@yahoo.com>
2019-08-13 12:05:13 -0500

    answered questions

---
+++ index.js
@@ -0,0 +1,21 @@
+//exercise 1
+let names = ["A",'B','C','D'];
+console.log(names);
+names.push("E");
+names.push("F");
+names.push("G");
+names.push("H");
+console.log(names);
+
+
+// //exercise 2
+
+let num = [1,2,3,4,5];
+console.log(num);
+
+//exercise 3
+let names1 = ['A','B','C','D'];
+console.log(names1);
+console.log(names1[2]);
+names1.splice(1,1);
+console.log(names1[2]);
